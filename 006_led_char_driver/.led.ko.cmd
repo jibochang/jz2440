013d20f2733a91d12fa68795e211348eed087e4a
daipeng <lildaipeng@outlook.com>
2024-01-02 17:53:41 +0800

    2024/1/2: 	006:cut file

--- 006_led_char_driver/.led.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/jz2440/006_led_char_driver/led.ko := arm-linux-ld -EL  -r -o /home/jz2440/006_led_char_driver/led.ko /home/jz2440/006_led_char_driver/led.o /home/jz2440/006_led_char_driver/led.mod.o
--- 006_led_char_driver/.tmp_versions/led.mod
+++
@@ -1,2 +0,0 @@
-/home/jz2440/006_led_char_driver/led.ko
-/home/jz2440/006_led_char_driver/led.o
